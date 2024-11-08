0515e8342f093cd620468f934f345a3b9f501a06
rituthori <ritu8473@gmail.com>
2024-11-08 21:26:15 +0800

    Response parity

--- src/main/java/sg/edu/nus/iss/delivery_service/service/DeliveryService.java
+++ src/main/java/sg/edu/nus/iss/delivery_service/service/DeliveryService.java
@@ -1,8 +1,6 @@
 package sg.edu.nus.iss.delivery_service.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
